727d8899132d6d3f959725ca3b7289aad1a6f010
Matheus Mota Tonini <138072866+motaxyz@users.noreply.github.com>
2024-04-18 22:39:19 -0300

    Criação do controle de usuário
    Criação do controller usuário

---
+++ controllers/usuario.js
@@ -0,0 +1,164 @@
+const db = require('../database/connection');
+
+module.exports = { 
+    async listarUsuarios(request, response) {
+        try {
+            // instrucoes sql
+            const sql = `SELECT
+            usu_id, usu_nome, usu_email, usu_dt_nasc, usu_senha, usu_tipo, usu_ativo = 1 AS usu_ativo
+            FROM usuarios
+            WHERE usu_ativo = 1;`;
+            // executa instruções sql e armazena o resultado na variavel usuários
+            const usuarios = await db.query(sql);
+            // armazena em uma variável o número de registros retornados
+            const nItens = usuarios[0].length;
+            
+            return response.status(200).json({
+                sucesso: true,
+                mensagem: 'Lista de Usuários.',
+                dados: usuarios[0],
+                nItens
+            })
+        } catch (error) {
+            return response.status(500).json({
+                sucesso: false,
+                mensagem: 'Erro na requisição.',
+                dados: error.message
+            });
+        }
+    },
+    async cadastrarUsuarios(request, response){
+        try {
+            // parâmetros recebidos no corpo da requisicao
+            const { usu_nome, usu_email, usu_dt_nasc, usu_senha, usu_tipo, usu_ativo } = request.body;
+            // instrução SQL
+            const sql = `INSET INTO usuarios
+                (usu_nome, usu_email, usu_dt_nasc, usu_senha, usu_tipo, usu_ativo)
+                VALUES (?, ?, ?, ?, ?, ?)`;
+            // definição dos dados a serem inseridos em um array
+            const values = [usu_nome, usu_email, usu_dt_nasc, usu_senha, usu_tipo, usu_ativo];
+            // execução da instrução sql passando os parâmetros
+            const execSql = await db.query(sql, values);
+            // identificação do ID do registro inserido
+            const usu_id = execSql[0].insertId;
+
+            return response.status(200).json({
+                sucesso: true,
+                mensagem: 'Cadastro de usuário efetuado com sucesso.',
+                dados: usu_id
+                //mensSql: execSql
+            });
+        } catch (error){
+            return response.status(500).json({
+                sucesso: false,
+                mensagem: 'Erro na requisição.',
+                dados: error.message
+            });
+        }
+    },
+    async editarUsuarios(request, response) {
+        try {
+            // parâmetros recebidos pelo corpo da requisição
+            const { usu_nome, usu_email, usu_dt_nasc, usu_senha, usu_tipo, usu_ativo } = request.body;
+            // parâmetro recebido pela URL via params ex: /usuario/1
+            const { usu_id } = request.params;
+            // instruções SQL
+            const sql = `UPDATE usuarios SET usu_nome = ?, usu_email = ?, usu_dt_nasc = ?, usu_senha = ?, usu_tipo = ?, usu_ativo = ? WHERE usu_id ?;`;
+            // preparo do array com dados que serão atualizados
+            const values = [usu_nome, usu_email, usu_dt_nasc, usu_senha, usu_tipo, usu_ativo, usu_id];
+            // execução e obtenção de confirmação da atualização realizada
+            const atualizaDados = await db.query(sql, values);
+        
+            return response.status(200).json({
+                sucesso: true,
+                mensagem: `Usuário ${usu_id} atualizado com sucesso!`,
+                dados: atualizaDados[0].affectedRows
+                // mensSql: atualizaDados
+            })
+        } catch (error) {
+            return response.status(500).json({
+                sucesso: false,
+                mensagem: 'Erro na requisição.',
+                dados: error.message
+            });
+        }
+    },
+    async apagarUsuarios(request, response){
+        try {
+            // parâmetro passado via URL na chamada da api pelo front-end
+            const { usu_id } = request.params;
+            // comando de exclusão
+            const sql = `DELETE FROM usuarios WHERE usu_id = ?`;
+            // array com parâmetros de exclusão
+            const values = [usu_id];
+            // executa instrução no banco de dados
+            const excluir = await db.query(sql, values);
+
+            return response.status(200).json({
+                sucesso: true,
+                mensagem: `Usuário ${usu_id} excluído com sucesso`,
+                dados: excluir[0].affectedRows
+            });
+        } catch (error) {
+            return response.status(500).json({
+                sucesso: false,
+                mensagem: 'Erro na requisição.',
+                dados: error.message
+            });
+        }
+    },
+    async ocultarUsuario(request, response) {
+        try {
+            const usu_ativo = false;
+            const { usu_id } = request.params;
+            const sql = `UPDATE usuarios SET usu_ativo = ?
+            WHERE usu_id = ?;`;
+            const values = [usu_ativo, usu_id];
+            const atualizacao = await db.query(sql, values);
+
+             return response.status(200).json({
+                sucesso: true,
+                mensagem: `Usuário ${usu_id} ocultado com sucesso`,
+                dados: atualizacao[0].affectedRows
+             });
+        } catch (error) {
+            return response.status(500).json({
+                sucesso: false,
+                mensagem: 'Erro na requisição.',
+                dados: error.message
+            });
+        }
+    },
+    async login(request, response) {
+        try {
+            const {usu_email, usu_senha} = request.body;
+
+            const sql = `SELECT usu_id, usu_nome, usu_tipo FROM usuarios WHERE usu_email = ? AND usu_senha = ? AND usu_ativo = 1;`;
+
+            const values = [usu_email, usu_senha];
+
+            const usuarios = await db.query(sql, values);
+            const nItens = usuarios[0].length;
+            
+            if (nItens < 1) {
+                return response.status(403).json({
+                    sucesso: false,
+                    mensagem: 'Login e/ou senha inválido.',
+                    dados: null,
+                });
+            }
+
+            return response.status(200).json({
+                sucesso: true,
+                mensagem: 'Login efetuado com sucesso',
+                dados: usuarios[0]
+            });
+        } catch (error) {
+            return response.status(500).json({
+                sucesso: false,
+                mensagem: 'Erro na requisição',
+                dados: error.message
+            });
+        }
+    },
+}
